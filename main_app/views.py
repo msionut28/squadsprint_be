ff0b4057e11d3d13e9a84be3e92f0b54554b70cc
msionut28 <moloceaionutt@gmail.com>
2023-11-24 17:04:18 +0200

    REDEPLOY

--- main_app/views.py
+++ main_app/views.py
@@ -41,7 +41,6 @@ class ManagerViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    
     def perform_create(self, serializer):
         serializer.save()
     # permission_classes = [permissions.IsAuthenticated]
